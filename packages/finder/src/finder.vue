f30a920201c21961ca6bc7bda7f55751f502d30d
Shine <ls@gokuai.cn>
2018-08-08 18:02:51 +0800

    新增文件预览

--- packages/finder/src/finder.vue
+++ packages/finder/src/finder.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="gk-finder">
         <div class="gk-finder-toolbar">
-            <gk-breadcrumb :show-nav="false" :data="navData" @navigator="clickBreadcrumb" label="filename" value="fullpath"></gk-breadcrumb>
+            <gk-breadcrumb :show-nav="false" :data="navList" @navigator="clickBreadcrumb" label="filename" value="fullpath"></gk-breadcrumb>
 
-            <div class="gk-finder-show-ops">
+            <div class="gk-finder-show-ops" v-show="!preview">
                 <gk-button icon="fa fa-sort" class="gk-finder-sort-button"></gk-button>
                 <gk-button-group plain class="gk-finder-view-mode">
                     <gk-button
@@ -20,51 +20,57 @@
         </div>
 
         <div class="gk-finder-content" :class="'gk-finder-view-' + viewMode">
-            <gk-thumbnail fit v-if="viewMode == 'thumbnail'" :loading="loading" :data="jsonData" :border="0" :selectedIndex="selectedIdx"
-                          @select="selectItem" @dblclick="dblclickItem">
-                <template slot-scope="props">
-                    <p>
-                        <img :src="props.thumb + '&size=128'"/>
-                    </p>
-                    <p>{{props.filename}}</p>
-                </template>
-            </gk-thumbnail>
-            <gk-table fit :loading="loading" show-checkbox show-header :data="jsonData" :selectedIndex="selectedIdx" :itemHeight="itemHeight"
-                      @select="selectItem" @dblclick="dblclickItem" v-else-if="viewMode === 'list'">
-                <gk-table-column checkbox :width="30" align="center"></gk-table-column>
-                <gk-table-column property="filename" label="文件名" sortable>
+            <template v-show="!preview">
+                <gk-thumbnail fit v-if="viewMode == 'thumbnail'" :loading="loading" :data="list" :border="0" :selectedIndex="selectedIndex"
+                              @select="selectItem" @dblclick="dblclickItem">
                     <template slot-scope="props">
-                        <div class="gk-finder-filename">
-                            <img :src="props.thumb + '&size=32'" height="16"/>{{props.filename}}
-                        </div>
+                        <p>
+                            <img :src="props.thumb + '&size=128'"/>
+                        </p>
+                        <p>{{props.filename}}</p>
                     </template>
-                </gk-table-column>
-                <gk-table-column property="last_dateline" label="最后修改" :formatter="formatDate" sortable
-                                 :width="180"></gk-table-column>
-                <gk-table-column property="filesize" label="大小" :formatter="formatSize" sortable
-                                 :width="100"></gk-table-column>
-                <gk-table-column :width="200"></gk-table-column>
-            </gk-table>
-            <gk-table fit :loading="loading" :data="jsonData" :itemHeight="itemHeight + 20" :selectedIndex="selectedIdx" @select="selectItem"
-                      @dblclick="dblclickItem" v-else>
-                <gk-table-column :width="30"></gk-table-column>
-                <gk-table-column property="filename" label="文件名" sortable>
-                    <template slot-scope="props">
-                        <div class="gk-finder-filename">
-                            <img :src="props.thumb + '&size=32'"/>
-                            <div>
-                                <p>{{props.filename}}</p>
-                                <p>
-                                    <span>{{props.last_member_name}}</span>
-                                    <span>{{formatDate(props.last_dateline)}}</span>
-                                    <span v-if="props.filesize">{{formatSize(props.filesize, props)}}</span>
-                                </p>
+                </gk-thumbnail>
+                <gk-table fit :loading="loading" show-checkbox show-header :data="list" :selectedIndex="selectedIndex" :itemHeight="itemHeight"
+                          @select="selectItem" @dblclick="dblclickItem" v-else-if="viewMode === 'list'">
+                    <gk-table-column checkbox :width="30" align="center"></gk-table-column>
+                    <gk-table-column property="filename" label="文件名" sortable>
+                        <template slot-scope="props">
+                            <div class="gk-finder-filename">
+                                <img :src="props.thumb + '&size=32'" height="16"/>{{props.filename}}
                             </div>
-                        </div>
-                    </template>
-                </gk-table-column>
-                <gk-table-column :width="200"></gk-table-column>
-            </gk-table>
+                        </template>
+                    </gk-table-column>
+                    <gk-table-column property="last_dateline" label="最后修改" :formatter="formatDate" sortable
+                                     :width="180"></gk-table-column>
+                    <gk-table-column property="filesize" label="大小" :formatter="formatSize" sortable
+                                     :width="100"></gk-table-column>
+                    <gk-table-column :width="200"></gk-table-column>
+                </gk-table>
+                <gk-table fit :loading="loading" :data="list" :itemHeight="itemHeight + 20" :selectedIndex="selectedIndex" @select="selectItem"
+                          @dblclick="dblclickItem" v-else>
+                    <gk-table-column :width="30"></gk-table-column>
+                    <gk-table-column property="filename" label="文件名" sortable>
+                        <template slot-scope="props">
+                            <div class="gk-finder-filename">
+                                <img :src="props.thumb + '&size=32'"/>
+                                <div>
+                                    <p>{{props.filename}}</p>
+                                    <p>
+                                        <span>{{props.last_member_name}}</span>
+                                        <span>{{formatDate(props.last_dateline)}}</span>
+                                        <span v-if="props.filesize">{{formatSize(props.filesize, props)}}</span>
+                                    </p>
+                                </div>
+                            </div>
+                        </template>
+                    </gk-table-column>
+                    <gk-table-column :width="200"></gk-table-column>
+                </gk-table>
+            </template>
+
+            <gk-slide v-if="preview" fit>
+                <iframe v-if="value && value.previewUrl" :src="value.previewUrl"></iframe>
+            </gk-slide>
         </div>
     </div>
 </template>
@@ -77,21 +83,25 @@
   import GkTableColumn from "../../table/src/table-column";
   import {timeToDate, bitSize} from "../../../src/common/util";
   import GkThumbnail from "../../thumbnail/src/thumbnail";
+  import GkSlide from "../../slide/src/slide";
 
   export default {
     name: 'GkFinder',
-    components: {GkThumbnail, GkTableColumn, GkTable, GkButtonGroup, GkButton, GkBreadcrumb},
+    components: {GkSlide, GkThumbnail, GkTableColumn, GkTable, GkButtonGroup, GkButton, GkBreadcrumb},
     props: {
-      'json-data': {
+      list: { //当前文件列表
         type: Array,
         required: true
       },
+      value: { //选中文件/文件夹
+        type: Object|null,
+        required: true
+      },
+      current: Object, //当前打开文件/文件夹
+      root: Object, //根文件夹或库
       'item-height': {
         type: Number,
         default: 42
-      },
-      'nav-data': {
-        type: Array
       },
       loading: Boolean
     },
@@ -99,10 +109,32 @@
       return {
         viewMode: 'default',
         selected: {},
-        selectedIdx: -1
+        preview: false,
+        navList: this.initNavs(),
+        selectedIndex: -1
       };
     },
+    watch: {
+      list: 'loadSuccess'
+    },
     methods: {
+      initNavs() {
+        let navs = [], path = '', fullpath = '';
+        this.root && navs.push(this.root);
+        if (this.current && this.current.fullpath) {
+          fullpath += this.current.fullpath + '/';
+        }
+        for (let i = 0; i < fullpath.length; i++) {
+          if (fullpath[i] === '/') {
+            navs.push({
+              filename: path.substring(path.lastIndexOf('/'), path.length),
+              fullpath: fullpath.substr(0, fullpath.length - 1)
+            });
+          }
+          path += fullpath[i];
+        }
+        return navs;
+      },
       formatDate(value) {
         return timeToDate(value * 1000);
       },
@@ -112,18 +144,30 @@
       handleViewMode(mode) {
         this.viewMode = mode;
       },
-      selectItem(data, index) {
-        this.selected = data;
-        this.selectedIdx = index;
-        this.$emit('select', data, index);
+      selectItem(file, index) {
+        this.selected = file;
+        this.selectedIndex = index;
+        this.$emit('input', file);
       },
-      dblclickItem(data, index) {
-        this.selected = data;
-        this.selectedIdx = index;
-        this.$emit('dblclick', data, index);
+      dblclickItem(file, index) {
+        this.selected = file;
+        this.selectedIndex = index;
+        this.navList.push(file);
+        this.$emit('enter', file, index);
+        if (!file.dir) {
+          this.showPreview();
+        }
       },
-      clickBreadcrumb(value, data, index) {
-        this.$emit('navigator', value, data, index);
+      clickBreadcrumb(value, file, index) {
+        this.navList = this.navList.slice(0, index + 1);
+        this.selectItem(null, -1);
+        this.$emit('navigator', value, file);
+      },
+      showPreview() {
+        this.preview = true;
+      },
+      loadSuccess() {
+        this.preview = false;
       }
     }
   }
---
+++ packages/slide/src/slide.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="gk-slide" :class="{'gk-slide-fit': fit}">
+        <slot></slot>
+    </section>
+</template>
+
+<script>
+  export default {
+    name: "gkSlide",
+    props: {
+      fit: Boolean
+    }
+  }
+</script>
